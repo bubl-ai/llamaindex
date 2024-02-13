7a09b3fe680e651ad349bbd34e2c4eb028fd65dd
Santiago Olivar <santiago.olivar.a@gmail.com>
2024-02-12 22:36:11 -0800

    first minimal version of setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llamaindex_project',
+    version='0.1.0',
+    author= 'Santiago Olivar',
+    author_email='contact.bubl.ai@gmail.com',
+    description='',
+    long_description=open('README.md').read(),  # Assuming you have a README.md file
+    long_description_content_type='text/markdown',
+    url='https://github.com/bubl-ai/llamaindex-project',
+    license='MIT',  # Choose an appropriate license for your package
+    classifiers=[
+        'Development Status ::Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'bubls'}
+)
