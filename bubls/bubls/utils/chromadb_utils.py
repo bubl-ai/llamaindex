8870c3ded8f7f2313267a17750e6f8d5bfd616b9
Santiago Olivar <santiago.olivar.a@gmail.com>
2024-02-27 06:38:46 +0000

    second version tryin to load index from disk

--- bubls/bubls/utils/chromadb_utils.py
+++ bubls/bubls/utils/chromadb_utils.py
@@ -1,26 +1,55 @@
 import os
 import chromadb
 
+# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
 
-def collection_from_folder():
-    # client = chromadb.PersistentClient(path="/llamaindexproject")
-    client = chromadb.PersistentClient(
-        path=os.path.join(os.environ["PWD"], "chroma_collections")
-    )
-    # Create a collection
-    collection = client.get_or_create_collection(name="my_collection22")
-    collection.add(
-        documents=[
-            "This is a document galileo figaro magnifico hey there is someone playing videogames down on the road smiling at everyone",
-            "This is another document",
-        ],
-        metadatas=[{"source": "my_source"}, {"source": "my_source"}],
-        ids=["id1", "id2"],
-    )
-
-    # some useful methods
-    collection.peek()  # returns a list of the first 10 items in the collection
-    collection.count()  # returns the number of items in the collection
+# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from IPython.display import Markdown, display
 
 
-collection_from_folder()
+class ConfigurableChromaIndex:
+    """Create a configurable Chroma index."""
+
+    def __init__(self, name_collection: str, path: str, embedding_model: str):
+        # create client and a new collection
+        db = chromadb.PersistentClient(
+            path=os.path.join(os.environ["WORKDIR"], "chroma_db")
+        )
+        chroma_collection = db.get_or_create_collection(name_collection)
+
+        # define embedding function
+        # embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+        # load documents
+        documents = SimpleDirectoryReader(
+            os.path.join(os.environ["WORKDIR"], path)
+        ).load_data()
+
+        # set up ChromaVectorStore and load in data
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+        storage_context = StorageContext.from_defaults(
+            vector_store=vector_store
+        )
+        index = VectorStoreIndex.from_documents(
+            documents,
+            storage_context=storage_context,  # , embed_model=embed_model
+        )
+
+        # Query Data
+        query_engine = index.as_query_engine()
+        response = query_engine.query("What did the author do growing up?")
+        display(Markdown(f"<b>{response}</b>"))
+
+        # save to disk
+
+        # index = VectorStoreIndex.from_vector_store(
+        #     vector_store,
+        #     embed_model=embed_model,
+        # )
+
+
+if __name__ == "__main__":
+    a = ConfigurableChromaIndex("aaa", "/data/williams_family/biographies")
